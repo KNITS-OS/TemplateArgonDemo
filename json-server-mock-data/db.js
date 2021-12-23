b38463b8e1547260422552e6c8e0afd5e88b3621
stefano.fiorenza <stefanofiorenza@gmail.com>
2021-12-23 16:30:26 +0200

    [Feature]: Json-Server with mock data examples

---
+++ json-server-mock-data/db.js
@@ -0,0 +1,12 @@
+const employees = require("./employees.js");
+const businessUnits = require("./business-units.js");
+const countries = require("./countries.js");
+const groups = require("./groups.js");
+
+
+module.exports = () => ({
+    employees,
+    businessUnits,
+    countries,
+    groups
+});
