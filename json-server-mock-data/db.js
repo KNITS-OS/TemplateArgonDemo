192ef715104583691a99b632abf920625681c6d6
tomimarkus991 <tomimarkusalber@gmail.com>
2021-12-27 10:07:59 +0200

    change json-server port

--- json-server-mock-data/db.js
+++ json-server-mock-data/db.js
@@ -3,10 +3,9 @@ const businessUnits = require("./business-units.js");
 const countries = require("./countries.js");
 const groups = require("./groups.js");
 
-
 module.exports = () => ({
-    employees,
-    businessUnits,
-    countries,
-    groups
+  employees,
+  businessUnits,
+  countries,
+  groups,
 });
