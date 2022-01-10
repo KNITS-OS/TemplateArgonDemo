ce5235120f00fe5f0748e74f35c42050b00039fc
tomimarkus991 <tomimarkusalber@gmail.com>
2022-01-10 09:42:29 +0200

    add groups crud

--- src/redux/groups/group.service.js
+++ src/redux/groups/group.service.js
@@ -4,6 +4,30 @@ const searchGroups = () => {
   return httpCommon.get(`/groups`);
 };
 
+const searchGroup = id => {
+  return httpCommon.get(`/groups?id=${id}`);
+};
+
+const createGroup = body => {
+  return httpCommon.post(`/groups`, body);
+};
+
+const updateGroup = (id, body) => {
+  return httpCommon.patch(`/groups/${id}`, body, {
+    headers: {
+      prefer: "resolution=merge-duplicates",
+    },
+  });
+};
+
+const deleteGroup = id => {
+  return httpCommon.delete(`/groups/${id}`);
+};
+
 export const groupService = {
   searchGroups,
+  searchGroup,
+  createGroup,
+  updateGroup,
+  deleteGroup,
 };
